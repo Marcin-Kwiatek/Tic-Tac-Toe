471421e15d6ccb1d93bbcc1fc620827efaf61a92
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-01-22 18:46:36 +0100

    adding all fields

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("good")
+let c0 = document.getElementById('0')
+let c1 = document.getElementById('1')
+let c2 = document.getElementById('2')
+
+let c3 = document.getElementById('3')
+let c4 = document.getElementById('4')
+let c5 = document.getElementById('5')
+
+let c6 = document.getElementById('6')
+let c7 = document.getElementById('7')
+let c8 = document.getElementById('8')
+
+c0.onclick  function addXO(){}
+
+function addXO () {
+    console.log("0")
+}
