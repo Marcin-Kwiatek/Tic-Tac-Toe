5fb104c5f3a015977e2c2f688030e9032714ed5e
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-01-29 20:34:30 +0100

    add circle and a cross jpg

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
     
 })
 
-let player1 = 'fa-circle-o'
-let player2 = 'fa-times'
+let player1 ="X"
+let player2 ="O"
 let round = 1
 let places = [...document.querySelectorAll(".XO")]
 places.forEach(XO => XO.addEventListener("click", choose))
 
 function choose(event){
     let turn = round % 2 ===0 ? player2 : player1
-    event.target.classList.add(turn)
+    event.target.idList.add(turn)
     round++
 }
 
