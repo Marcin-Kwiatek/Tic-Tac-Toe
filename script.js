41c43f66776a8ee3af2e08576a8ff03082e7901a
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-01-30 18:45:56 +0100

    new js add working X and O

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event => {
     
-})
+}))
+let p0 = document.getElementById("p0")
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
 
-let player1 ="X"
-let player2 ="O"
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+let p5 = document.getElementById('p5')
+
+let p6 = document.getElementById('p6')
+let p7 = document.getElementById('p7')
+let p8 = document.getElementById('p8')
+
+p0.addEventListener("click", function() { change(0) })
+p1.addEventListener("click", function() { change(1) })
+p2.addEventListener("click", function() { change(2) })
+
+p3.addEventListener("click", function() { change(3) })
+p4.addEventListener("click", function() { change(4) })
+p5.addEventListener("click", function() { change(5) })
+
+p6.addEventListener("click", function() { change(6) })
+p7.addEventListener("click", function() { change(7) })
+p8.addEventListener("click", function() { change(8) })
+
+let player1 = "X"
+let player2 = "O"
 let round = 1
-let places = [...document.querySelectorAll(".XO")]
-places.forEach(XO => XO.addEventListener("click", choose))
 
-function choose(event){
-    let turn = round % 2 ===0 ? player2 : player1
-    event.target.idList.add(turn)
+function change(nr){
+    let turn = round % 2 === 0 ? player2 : player1
+    let place = document.getElementById("p"+nr)
+    place.innerHTML=(turn)
     round++
 }
-
